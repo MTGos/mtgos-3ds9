e63320f97c5c224a56251105c2fefac9ef16b79a
Morten Delenk <morten@dark32.cf>
2017-04-29 11:25:19 +0000

    fixed x86_64

--- kernel/arch/x86_64/pc/start.cpp
+++ kernel/arch/x86_64/pc/start.cpp
@@ -1,8 +1,22 @@
+#include <config.h>
+#include <multiboot.h>
+#ifndef ENABLE_FRAMEBUFFER
 #include "../../../hw/pc/cgaterm/cgaterm.hpp"
+#else
+#include "../../../hw/pc/vesafb/vesafb.hpp"
+#endif
 #include <base.hpp>
+static multiboot_info_t *mb_info;
+#ifndef ENABLE_FRAMEBUFFER
 CGATerm term;
+#else
+VESAfb term(mb_info);
+#endif
 void main();
-extern "C" void start() { main(); }
+extern "C" void start(int eax, multiboot_info_t *ebx) {
+    mb_info = ebx;
+    main();
+}
 void drivers_init() {
     setMainTTY(&term);
     --term;
